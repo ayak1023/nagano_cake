8052e17064b26dc8357118646d8ccfdd10b5369a
EC2 Default User <ec2-user@ip-172-31-23-255.ap-northeast-1.compute.internal>
2024-12-06 10:33:33 +0000

    [Add]Model_Genre

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,3 @@
 class Item < ApplicationRecord
-  def change
-    create_table :items do |t|
-      t.string :name, null: false
-      t.text :introduction, null: false
-      t.integer :price, null: false
-      t.integer :genre_id, null: false
-      t.boolean :is_active, null: false
-      t.timestamps
-    end
-  end
+
 end
