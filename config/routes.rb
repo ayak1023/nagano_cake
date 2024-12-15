2cd0d1e46e06101e99d3dab8c18513c3ab2a4d37
EC2 Default User <ec2-user@ip-172-31-23-255.ap-northeast-1.compute.internal>
2024-12-15 03:36:41 +0000

    [Add]ヘッダーのリンク

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+  get 'about', to: 'homes#about', as: :about
 end
